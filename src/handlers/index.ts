879ab2e49bf998ac710b83cdc68bda2f85073bac
DDH-WEB <ddhwed@gmail.com>
2026-01-07 11:08:06 -0500

    Proceso

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,34 +2,49 @@ import { Request, Response } from 'express'
 import { validationResult } from 'express-validator'
 import slug from 'slug'
 import User from "../model/User"
-import { hashPassword } from '../utils/auth'
+import { checkPassword, hashPassword } from '../utils/auth'
 
-export const createAccount = async (req : Request, res:Response) => {
-    //Manejo de Errores
-    let errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()})
-    }
-
-    const {email, password} = req.body
-    const userExist = await User.findOne({email})
+export const createAccount = async (req: Request, res: Response) => {
+    const { email, password } = req.body
+    const userExist = await User.findOne({ email })
     if (userExist) {
         const error = new Error('El Usuario ya Esta Registrado')
-        return res.status(409).json({error : error.message})
+        return res.status(409).json({ error: error.message })
     }
 
     const handle = slug(req.body.handle, '')
-    const handleExist = await User.findOne({handle})
-    if(handleExist) {
+    const handleExist = await User.findOne({ handle })
+    if (handleExist) {
         const error = new Error('Nombre no Disponible')
-        return res.status(409).json({error: error.message})
+        return res.status(409).json({ error: error.message })
     }
 
     const user = new User(req.body)
     user.password = await hashPassword(password)
     user.handle = handle
-    
+
     await user.save()
 
     res.status(201).send('Usuario Registrado Correctamente')
+}
+
+export const login = async (req: Request, res: Response) => {
+
+    const { email, password } = req.body
+    //revisar si el usuario existe
+    const user = await User.findOne({ email })
+    
+    if(!user) {
+        const error = new Error('El usuario no existe')
+        return res.status(404).json({ error: error.message })
+    }
+
+    //comprobar su password
+    const isPasswordCorrect = await checkPassword(password, user.password as string)
+    if(!isPasswordCorrect) {
+        const error = new Error('Contraseña Incorrecta')
+        return res.status(401).json({ error: error.message })
+    }
+
+    res.status(200).json({ message: 'Usuario Autenticado Correctamente' })
 }
--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,40 @@
-import { Router } from 'express'
+import { Router, type RequestHandler } from 'express'
 import { body } from 'express-validator'
-import { createAccount } from './handlers'
+import { createAccount, login } from './handlers'
+import { handleInputErrors } from './middleware/validation'
 
 const router = Router()
 
 // Autenticación y Registro
-router.post('/auth/register', 
+router.post('/auth/register',
     body('handle')
         .notEmpty()
         .trim()
         .withMessage('El Handle no puede ir Vació'),
+    body('name')
+        .notEmpty()
+        .trim()
+        .withMessage('El Nombre no puede ir Vació'),
+    body('email')
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('El Email no es válido'),
+    body('password')
+        .isLength({ min: 8 })
+        .withMessage('La Contraseña debe tener al menos 8 caracteres'),
+    handleInputErrors,
     createAccount
 )
 
+router.post('/auth/login',
+    body('email')
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('El Email no es válido'),
+    body('password')
+        .notEmpty()
+        .withMessage('La Contraseña es Obligatoria'),
+    handleInputErrors,
+    login
+)
 export default router
